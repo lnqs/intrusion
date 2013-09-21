4efffd7d69105af5ea492360bb2c2df885ed0638
Lynx <simon.schoenfeld@googlemail.com>
2013-09-22 00:43:03 +0200

    Removed lighting
    Since the shader was that slow on my computer, that it didn't make fun
    to watch the camera-transitions, the most resource-intensive-code was
    removed -- the lighting. Since normals had to be calculated, some
    additional rays had to be cast. To avoid this overhead, the
    fragment-color is calculated from the number of steps needed to find a
    point belonging to the fractal now, what's looking pretty nice, too.
    This also has the nice benefit, that the shader cound be heavily
    simplified.

--- fragment.glsl
+++ fragment.glsl
@@ -3,24 +3,17 @@ varying vec3 p;
 uniform mat3 orientation;
 uniform vec3 position;
 
-uniform float box_scale = -2.5;
-uniform float box_radius = 3.0;
-uniform float sphere_radius = 3.0;
+uniform float box_scale;
+uniform float box_radius;
+uniform float sphere_radius;
 
 const float eye_distance = 2.0;
 const float max_distance = 15.0;
 const float accuracy = 0.0005;
+const vec3 object_glow = vec3(0.23, 0.21, 0.14);
 
-const float normal_eps = 0.0005;
-
-// We could define this in CPU-code with the GL-lighting-functions,
-// but I don't see a benefit while it would be more code.
-vec3 light_position = position; // Not const since position isn't
-const vec3 light_diffuse = vec3(0.5, 0.5, 0.5);
-const vec3 light_specular = vec3(0.5, 0.5, 0.5);
-
-const vec3 object_color = vec3(1.0, 1.0, 0.66);
-const float object_shininess = 10.0;
+// I have no idea why, but when I remove this declaration the shader doesn't compile o.O
+const float ignore;
 
 const float box_de_factor = 1.0;
 const int max_iterations = 12;
@@ -39,23 +32,10 @@ float distance_estimate(in vec3 point)
     return length(n.xyz) / n.w * box_de_factor;
 }
 
-vec3 find_normal(in vec3 point)
-{
-    vec2 epsilon = vec2(normal_eps, 0.0);
-
-    vec3 normal;
-    normal.x = distance_estimate(point - epsilon.xyy) - distance_estimate(point + epsilon.xyy);
-    normal.y = distance_estimate(point - epsilon.yxy) - distance_estimate(point + epsilon.yxy);
-    normal.z = distance_estimate(point - epsilon.yyx) - distance_estimate(point + epsilon.yyx);
-    normal = normalize(normal);
-
-    return normal;
-}
-
-bool intersects(in vec3 ray_origin, in vec3 ray_direction, out vec3 hitpoint, out vec3 normal, out int steps)
+int find_intersection(in vec3 ray_origin, in vec3 ray_direction)
 {
     float distance = 0.0;
-    steps = 0;
+    int steps = 0;
 
     while (distance < max_distance)
     {
@@ -64,42 +44,21 @@ bool intersects(in vec3 ray_origin, in vec3 ray_direction, out vec3 hitpoint, ou
 
         if (next_step < accuracy)
         {
-            hitpoint = point_to_check;
-            normal = find_normal(hitpoint);
-            return true;
+            return steps;
         }
 
         distance += next_step;
         steps++;
     }
 
-    return false;
-}
-
-void light(in vec3 point, in vec3 normal, inout vec3 color)
-{
-    vec3 light = normalize(light_position - point);
-    vec3 diffuse = max(dot(normal, -light), 0.0) * light_diffuse;
-    vec3 specular = light_specular * pow(max(dot(normalize(reflect(light, normal)), -light), 0.0), object_shininess);
-    color *= diffuse + specular;
+    return steps;
 }
 
 void main()
 {
     vec3 ray = normalize(orientation * (p + vec3(0.0, 0.0, -eye_distance)));
 
-    vec3 hitpoint;
-    vec3 normal;
-    int steps;
-    if (intersects(position, ray, hitpoint, normal, steps))
-    {
-        vec3 color = object_color;
-        light(hitpoint, normal, color);
-        gl_FragColor = vec4(color.x, color.y, color.z, 1.0);
-    }
-    else
-    {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-    }
+    int steps = find_intersection(position, ray);
+    gl_FragColor = vec4(object_glow * steps / 100.0, 1.0);
 }
 
