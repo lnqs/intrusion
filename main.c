12cfd00bd9aed841b7867cb6c5b272f8069fce0c
Lynx <simon.schoenfeld@googlemail.com>
2013-09-29 17:03:41 +0200

    Prettier camera movement
    Changed the keypoints to have prettier scenes

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 #include "shader_code.h"
 #include "sound.h"
 
-static const int resolution_x = 640;
-static const int resolution_y = 480;
+static const int resolution_x = 800;
+static const int resolution_y = 600;
 static bool fullscreen = false;
 static char* window_caption = "Planeshift";
 static const float movement_speed = 0.5;
@@ -29,7 +29,7 @@ struct keypoint
 static const struct keypoint keypoints[] = {
     {
         .time = 0,
-        .position = { 0.0, 0.0, 4.3 },
+        .position = { 0.00, 0.00, 4.30 },
         .orientation = MATRIX3_IDENTITY,
         .box_scale = -5.0,
         .box_radius = 3.0,
@@ -37,32 +37,31 @@ static const struct keypoint keypoints[] = {
 
     },
     {
-        .time = 8000,
-        .position = { 0.0, -8.0, 4.3 },
-        .orientation = {
-            { 1.08, -0.08,  0.00 },
-            { 0.05,  0.58, -0.80 },
-            { 0.07,  0.80,  0.60 }
-
-        },
-        .box_scale = -5.0,
-        .box_radius = 3.0,
-        .sphere_radius = 3.0
-    },
-    {
-        .time = 20000,
-        .position = { 0.0, -5.0, 7.5 },
+        .time = 10 * 1000,
+        .position = { 0.23, -5.74, 7.96 },
         .orientation = {
             {  0.96, -0.04,  0.26 },
             {  0.24,  0.54, -0.80 },
-            { -0.11,  0.83,  0.53 },
+            { -0.11,  0.83,  0.53 }
         },
         .box_scale = -5.0,
         .box_radius = 4.0,
         .sphere_radius = 3.0
     },
     {
-        .time = 36000,
+        .time = 20 * 1000,
+        .position = { 2.51, -4.36, 6.77 },
+        .orientation = {
+            {  0.60, -0.00, -0.17 },
+            {  0.26,  0.03, -0.54 },
+            { -0.39,  0.41,  0.34 }
+        },
+        .box_scale = -4.1,
+        .box_radius = 3.6,
+        .sphere_radius = 2.2
+    },
+    {
+        .time = 30 * 1000,
         .position = { 5.09, -1.66, 5.23 },
         .orientation = {
             { -0.03,  0.08, -0.99 },
@@ -74,7 +73,7 @@ static const struct keypoint keypoints[] = {
         .sphere_radius = 0.75
     },
     {
-        .time = 46000,
+        .time = 40 * 1000,
         .position = { 5.06, 3.74, 5.29 },
         .orientation = {
             { -0.30,  0.93, -0.08 },
@@ -86,7 +85,7 @@ static const struct keypoint keypoints[] = {
         .sphere_radius = 0.75
     },
     {
-        .time = 55000,
+        .time = 50 * 1000,
         .position = { 5.08, 8.65, 5.31 },
         .orientation = {
             { -0.02,  0.85, 0.47 },
@@ -98,7 +97,7 @@ static const struct keypoint keypoints[] = {
         .sphere_radius = 0.75
     },
     {
-        .time = 70000,
+        .time = 60 * 1000,
         .position = { 0.33, 7.38, -1.03 },
         .orientation = {
             { 0.54,  0.76,  0.23 },
@@ -110,7 +109,7 @@ static const struct keypoint keypoints[] = {
         .sphere_radius = -2.0
     },
     {
-        .time = 80000,
+        .time = 70 * 1000,
         .position = { -0.24, 5.19, -0.57 },
         .orientation = {
             { -0.04 , 0.90, -0.31 },
@@ -122,7 +121,7 @@ static const struct keypoint keypoints[] = {
         .sphere_radius = -2.0
     },
     {
-        .time = 90000,
+        .time = 80 * 1000,
         .position = { -0.14, 3.67, 0.15 },
         .orientation = {
             { -0.04,  0.94, -0.05 },
@@ -134,7 +133,7 @@ static const struct keypoint keypoints[] = {
         .sphere_radius = -2.0
     },
     {
-        .time = 100000,
+        .time = 90 * 1000,
         .position = { -0.25, 3.33, -0.70 },
         .orientation = {
             {  0.01,  0.93,  0.17 },
@@ -146,7 +145,7 @@ static const struct keypoint keypoints[] = {
         .sphere_radius = -2.0
     },
     {
-        .time = 110000,
+        .time = 100 * 1000,
         .position = { -0.40, 3.62, -2.33 },
         .orientation = {
             { -0.05,  0.91,  0.24 },
@@ -158,7 +157,7 @@ static const struct keypoint keypoints[] = {
         .sphere_radius = -2.0
     },
     {
-        .time = 120000,
+        .time = 110 * 1000,
         .position = { -0.69, 3.02, -3.72 },
         .orientation = {
             { -0.14,  0.65, -0.67 },
@@ -170,15 +169,64 @@ static const struct keypoint keypoints[] = {
         .sphere_radius = -2.0
     },
     {
-        .time = 150000,
-        .position = { 10.34, 3.07, -4.24 },
+        .time = 120 * 1000,
+        .position = { -1.05, 2.77, -4.57 },
         .orientation = {
-            {  0.05, -0.20, -0.91 },
-            {  0.08, -0.91,  0.20 },
-            { -0.93, -0.07, -0.04 }
+            {  0.50,  0.79, -0.05 },
+            {  0.78, -0.49, -0.11 },
+            { -0.15,  0.02, -0.92 }
         },
-        .box_scale = -3.0,
-        .box_radius = 5.0,
+        .box_scale = -2.5,
+        .box_radius = 2.0,
+        .sphere_radius = -2.0
+    },
+    {
+        .time = 130 * 1000,
+        .position = { -1.33, -0.22, -4.94 },
+        .orientation = {
+            { -0.02,  0.66, -0.66 },
+            {  0.93, -0.01, -0.06 },
+            { -0.07, -0.66, -0.65 }
+        },
+        .box_scale = -2.5,
+        .box_radius = 2.0,
+        .sphere_radius = -2.0
+    },
+    {
+        .time = 140 * 1000,
+        .position = { -0.88, -0.14, -3.26 },
+        .orientation = {
+            { -0.01,  0.89,  0.28 },
+            {  0.93,  0.02, -0.00 },
+            { -0.03,  0.28, -0.89 }
+        },
+        .box_scale = -2.5,
+        .box_radius = 2.0,
+        .sphere_radius = -2.0
+    },
+    {
+        .time = 150 * 1000,
+        .position = { -2.48, -0.08, -1.67 },
+        .orientation = {
+            { -0.03, 0.85,  0.39 },
+            {  0.93, 0.03,  0.00 },
+            { -0.03, 0.39, -0.85 }
+        },
+        .box_scale = -2.5,
+        .box_radius = 2.0,
+        .sphere_radius = -2.0
+    },
+
+    {
+        .time = 160 * 1000,
+        .position = { -3.67, -0.15, -0.74 },
+        .orientation = {
+            { -0.01,  0.14,  0.92 },
+            {  0.93, -0.00,  0.01 },
+            { -0.01,  0.92, -0.15 }
+        },
+        .box_scale = -2.5,
+        .box_radius = 2.0,
         .sphere_radius = -2.0
     }
 };
