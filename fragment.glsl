67cc4e6af02f2c82e0bd88c91a6c93aea7100066
Lynx <simon.schoenfeld@googlemail.com>
2013-10-18 17:34:13 +0200

    Using defines instead of constants in shader
    Since the shader-code is shipped as text, this shrinks the binary.

--- fragment.glsl
+++ fragment.glsl
@@ -2,6 +2,13 @@
 
 ESCAPED(#version 330)
 
+#define EYE_DISTANCE 2.0
+#define MAX_DISTANCE 15.0
+#define ACCURACY 0.0005
+#define OBJECT_GLOW vec3(0.23, 0.21, 0.14)
+
+#define MAX_ITERATIONS 12
+
 in vec3 p;
 out vec4 color;
 
@@ -9,26 +16,18 @@ uniform vec3 x; // position passed by CPU-code
 uniform mat3 o; // orientation passed by CPU-code
 uniform vec3 f; // fractal-parameters passed by CPU-code. It's (box_scale, box_radius, sphere_radius).
 
-const float eye_distance = 2.0;
-const float max_distance = 15.0;
-const float accuracy = 0.0005;
-const vec3 object_glow = vec3(0.23, 0.21, 0.14);
-
-const float box_de_factor = 1.0;
-const int max_iterations = 12;
-
 float distance_estimate(in vec3 point)
 {
     vec4 n = vec4(point, 1.0);
 
-    for (int i = 0; i < max_iterations; i++)
+    for (int i = 0; i < MAX_ITERATIONS; i++)
     {
         n.xyz = clamp(n.xyz, -f.y, f.y) * 2.0 - n.xyz;
         n *= max(f.z * f.z / dot(n.xyz, n.xyz), 1.0);
         n = f.x * n + vec4(point, sign(f.x * n.w));
     }
 
-    return length(n.xyz) / n.w * box_de_factor;
+    return length(n.xyz) / n.w;
 }
 
 int find_intersection(in vec3 ray_origin, in vec3 ray_direction)
@@ -36,12 +35,12 @@ int find_intersection(in vec3 ray_origin, in vec3 ray_direction)
     float distance = 0.0;
     int steps = 0;
 
-    while (distance < max_distance)
+    while (distance < MAX_DISTANCE)
     {
         vec3 point_to_check = ray_origin + ray_direction * distance;
         float next_step = distance_estimate(point_to_check);
 
-        if (next_step < accuracy)
+        if (next_step < ACCURACY)
         {
             return steps;
         }
@@ -57,11 +56,11 @@ void main()
 {
     if (int(mod(gl_FragCoord.y, 2.0)) == 0)
     {
-        vec3 ray = normalize(o * (p + vec3(0.0, 0.0, -eye_distance)));
+        vec3 ray = normalize(o * (p + vec3(0.0, 0.0, -EYE_DISTANCE)));
         int steps = find_intersection(x, ray);
-        color.r = object_glow.r * steps / 85.0;
-        color.g = object_glow.g * steps / 85.0;
-        color.b = object_glow.b * steps / 85.0;
+        color.r = OBJECT_GLOW.r * steps / 85.0;
+        color.g = OBJECT_GLOW.g * steps / 85.0;
+        color.b = OBJECT_GLOW.b * steps / 85.0;
         color.a = 1.0;
     }
 }
