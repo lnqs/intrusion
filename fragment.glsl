1f2c412659ea3d674c3772249929aaaca3257edf
Lynx <simon.schoenfeld@googlemail.com>
2013-10-19 01:19:43 +0200

    Work around shader minifier bug
    There's a bug in shader-minifier, causing identifier to be used multiple
    times. Worked around it by restructuring the code.

--- fragment.glsl
+++ fragment.glsl
@@ -19,20 +19,6 @@ float rand(in vec2 seed)
     return float((36969u * uint(seed.x) << 16u) + (18000u * uint(seed.y))) * 2.328306435454494e-10;
 }
 
-float distance_estimate(in vec3 point)
-{
-    vec4 n = vec4(point, 1.0);
-
-    for (int i = 0; i < MAX_ITERATIONS; i++)
-    {
-        n.xyz = clamp(n.xyz, -f.y, f.y) * 2.0 - n.xyz;
-        n *= max(f.z * f.z / dot(n.xyz, n.xyz), 1.0);
-        n = f.x * n + vec4(point, sign(f.x * n.w));
-    }
-
-    return length(n.xyz) / n.w;
-}
-
 int find_intersection(in vec3 ray_origin, in vec3 ray_direction)
 {
     float dist = 0.0;
@@ -40,7 +26,23 @@ int find_intersection(in vec3 ray_origin, in vec3 ray_direction)
 
     while (dist < MAX_DISTANCE)
     {
-        float next_step = distance_estimate(ray_origin + ray_direction * dist);
+        // Here starts, what was the function distance_estimate() once.
+        // Due to a bug in shader_minifier, which was using the same name for
+        // two functions, I had to inline it.
+        // The original function took 'point' as parameter...
+        vec3 point = ray_origin + ray_direction * dist;
+        vec4 n = vec4(point, 1.0);
+
+        for (int i = 0; i < MAX_ITERATIONS; i++)
+        {
+            n.xyz = clamp(n.xyz, -f.y, f.y) * 2.0 - n.xyz;
+            n *= max(f.z * f.z / dot(n.xyz, n.xyz), 1.0);
+            n = f.x * n + vec4(point, sign(f.x * n.w));
+        }
+
+        // ... and returned this rhs expression, that was assign to 'next_step'
+        // by the caller.
+        float next_step = length(n.xyz) / n.w;
 
         if (next_step < ACCURACY)
         {
@@ -60,11 +62,12 @@ void main()
 
     if (c.r == 0.0 && int(mod(gl_FragCoord.y, 2.0)) == 0)
     {
-        // Since the following line of code isn't really readable -- it's the same as this:
+        // Since the following line of code isn't really readable -- it's the same as this
         //     float skew = rand(gl_FragCoord.xy) * e.x + 1.0 - e.x;
         //     vec3 ray = normalize(o * (p * skew + vec3(0.0, 0.0, -EYE_DISTANCE)));
         //     int steps = find_intersection(x, ray);
         //     c = OBJECT_GLOW * steps / 85.0;
+        // but smaller.
         c = OBJECT_GLOW * find_intersection(x,
             normalize(o *
                 (p * (rand(gl_FragCoord.xy) * e.x + 1.0 - e.x)
